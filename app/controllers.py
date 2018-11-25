4210ced6788655b665e6b48f250bbfb31a8bb9cf
Azu Nwaokobia <azdonald@yahoo.co.uk>
2018-11-24 23:32:31 -0500

    Allow search for a book of the bible with the post method

--- app/controllers.py
+++ app/controllers.py
@@ -1,25 +1,39 @@
 from flask_restful import Resource
-from flask import jsonify
+from flask import jsonify, request
 from app.data import DataLoader
 import json
 from datetime import datetime
+
 
 
 class DailyReading(Resource):
     def get(self):
         dailyReading = None
         currentDate = datetime.now()
-        bibleDate = DataLoader()
+        bibleData = DataLoader()
         timeOfTheDay = currentDate.timetuple().tm_hour
-        bibleReading = bibleDate.getDailyReading(currentDate.timetuple().tm_yday)
+        bibleReading = bibleData.getDailyReading(currentDate.timetuple().tm_yday)
         if (timeOfTheDay < 12 ):
             dailyReading = bibleReading['FirstReading']
         elif (timeOfTheDay >= 12 and timeOfTheDay < 17):
             dailyReading = bibleReading['SecondReading']
         else:
             dailyReading = bibleReading['ThirdReading']
-        return jsonify(dailyReading)
+        bibleVerse = dailyReading.split('.')
+        chapters = None
+        if '-' in bibleVerse[1]:
+            chapters = bibleVerse[1].split('-')
+        startChapter = chapters[0] if chapters else bibleVerse[1].strip()
+        return jsonify(bibleData.getReading(bibleVerse[0], startChapter))
 
 class BibleReading(Resource):
     def get(self):
-        pass
+        book = 'Genesis'
+        bibleData = DataLoader()
+        return jsonify(bibleData.getReading(book))
+
+    def post(self):
+        params = request.get_json()
+        book = params['book']
+        bibleData = DataLoader()
+        return jsonify(bibleData.getReading(book))
--- app/data.py
+++ app/data.py
@@ -8,3 +8,39 @@ class DataLoader(object):
 
     def getDailyReading(self, dayOfTheYear):
         return self.dailyReads[dayOfTheYear]
+
+    def loadBible(self):
+        bible = None
+        with open('msg.txt') as f:
+            bible = json.loads(f.read())
+        return bible
+
+    def getReading(self, book, startChapter=1, endChapter=None):
+        bible = self.loadBible()
+        chapters = self.getChapters(bible, book)
+        verses = self.getVerses(chapters, startChapter, endChapter)
+        return verses
+        
+        
+    def getChapters(self, bible, book):
+        chapters = None
+        book = book.strip()
+        for b in bible['Books']:
+            if b['BookName'].startswith(book):
+                chapters = b['BookChapter']
+                break
+        return chapters
+    
+    def getVerses(self, chapters, startChapter=1, endChapter=None):
+        verses = None
+        if endChapter:
+            print(endChapter)
+            verses = chapters[int(startChapter) - 1: int(endChapter)]
+        else:
+            for c in chapters:
+                if c['ChapterId'] == int(startChapter) :
+                    verses = c['ChapterVerses']
+                    break
+        print(startChapter)
+        return verses
+
